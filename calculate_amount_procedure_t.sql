12f0dab32f33ef83d00e39d0ab2363bccab83aa5
sudbasnet <shadefill@gmail.com>
2016-07-30 17:55:15 +0545

    final insert into data_final_amount, now just need to rename the details table

--- calculate_amount_procedure_t.sql
+++ calculate_amount_procedure_t.sql
@@ -8,7 +8,7 @@ begin
 	declare v_tier_code varchar(200);
 	declare v_uniqueid varchar(200);
 	declare v_operator_code varchar(200);
-	declare v_service_code varchar(200);
+	declare v_serviceid varchar(200);
 	declare v_product_id varchar(200);
 	declare v_start_date date;
 	declare v_end_date date;
@@ -30,15 +30,12 @@ begin
 	declare back_to_first_flag boolean;
 	declare done boolean;
 
-/*-- these cursors can be changed to suit the need of application --*/
-/*		select uniqueid, group_concat(callvolumeid separator ',') as callvolumeid, sum(duration) as duration, rate_profile_detail_id, rate_type, is_vbr 
-from vw_call_volume_link group by uniqueid, rate_type, is_vbr; */
 	declare maincursor cursor for 
 		select
 		group_concat(uniqueid separator ',') as uniqueid, 
 		group_concat(tier_code separator '-') as tier_code,
 		operator_code, 
-		service_code,
+		serviceid,
 		product_code,
 		date_format(start_date, '%d/%m/%Y') as start_date,
 		date_format(end_date, '%d/%m/%Y') as end_date,
@@ -50,7 +47,7 @@ from vw_call_volume_link group by uniqueid, rate_type, is_vbr; */
 		vw_call_volume_link 
 	group by 
 		operator_code, 
-		service_code,
+		serviceid,
 		product_code,
 		date_format(start_date, '%d/%m/%Y'),
 		date_format(end_date, '%d/%m/%Y'),
@@ -66,7 +63,7 @@ open maincursor;
 
 notvbr_loop: loop
 
-	fetch maincursor into v_uniqueid, v_tier_code, v_operator_code, v_service_code, v_product_id, v_start_date, v_end_date, v_callduration, v_rateprofileid, v_rate_type, v_is_vbr;
+	fetch maincursor into v_uniqueid, v_tier_code, v_operator_code, v_serviceid, v_product_id, v_start_date, v_end_date, v_callduration, v_rateprofileid, v_rate_type, v_is_vbr;
 
 if done then
     close maincursor;
@@ -90,7 +87,7 @@ fetch cur_calc into v_ceiling, v_rate, v_amount, v_hybrid_type;
     
 /*	set @v_query= concat('insert into data_final_details_t values(''',v_uniqueid,''',''',v_rate_type,''',', ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',v_amount,',''',ifnull(v_hybrid_type,'null'),''',',v_amount,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',null)'); */
 
-	set @v_query=concat('insert into data_final_details_t values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',ifnull(v_callduration,0),',''',ifnull(v_hybrid_type,'null'),''',',v_amount,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_service_code,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
+	set @v_query=concat('insert into data_final_details_t(uniqueid,tier,rate_type,ceiling,rate,call_minutes,hybrid_type,amount,created_at,rate_profile_detail_id,is_vbr,billing_operator,service_id,product,start_date,end_date,deleted_at) values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',ifnull(v_callduration,0),',''',ifnull(v_hybrid_type,'null'),''',',v_amount,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_serviceid,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
 
 	insert into query_log values(v_uniqueid, @v_query, sysdate()); 
 	prepare stmt from @v_query;
@@ -106,7 +103,7 @@ fetch cur_calc into v_ceiling, v_rate, v_amount, v_hybrid_type;
 		leave notvbr_loop;
 	end if;	
 /*	set @v_query= concat('insert into data_final_details_t values(''',v_uniqueid,''',''',v_rate_type,''',', ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',round(v_amount*v_rate,2),',''',ifnull(v_hybrid_type,'null'),''',',v_amount,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',null)');*/
-	set @v_query=concat('insert into data_final_details_t values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',ifnull(v_callduration,0),',''',ifnull(v_hybrid_type,'null'),''',',v_amount,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_service_code,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
+	set @v_query=concat('insert into data_final_details_t(uniqueid,tier,rate_type,ceiling,rate,call_minutes,hybrid_type,amount,created_at,rate_profile_detail_id,is_vbr,billing_operator,service_id,product,start_date,end_date,deleted_at) values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',ifnull(v_callduration,0),',''',ifnull(v_hybrid_type,'null'),''',',v_amount,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_serviceid,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
 
 	insert into query_log values(v_uniqueid, @v_query, sysdate());
 	prepare stmt from @v_query;
@@ -141,7 +138,7 @@ if v_rate_type='incremental' then
 		if amountapplied>0 then
 			/*		set @v_query= concat('insert into data_final_details_t values(''',v_uniqueid,''', ''',v_rate_type,''', ',v_ceiling,', ',v_rate,', ',callsapplied,', ''', v_hybrid_type,''', ',amountapplied,', sysdate(), ''',v_rateprofileid,''',''',v_is_vbr,''',null)');	*/
 			
-			set @v_query=concat('insert into data_final_details_t values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',callsapplied,',''',ifnull(v_hybrid_type,'null'),''',',amountapplied,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_service_code,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
+			set @v_query=concat('insert into data_final_details_t(uniqueid,tier,rate_type,ceiling,rate,call_minutes,hybrid_type,amount,created_at,rate_profile_detail_id,is_vbr,billing_operator,service_id,product,start_date,end_date,deleted_at) values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',callsapplied,',''',ifnull(v_hybrid_type,'null'),''',',amountapplied,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_serviceid,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
 			insert into query_log values(v_uniqueid, @v_query, sysdate());				
 			prepare stmt from @v_query;
 			execute stmt;
@@ -207,7 +204,7 @@ if v_rate_type='hybrid' then
 	end if;
 /*----------------------------------*/
 	if amountapplied>0 then
-			set @v_query=concat('insert into data_final_details_t values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',callsapplied,',''',ifnull(v_hybrid_type,'null'),''',',amountapplied,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_service_code,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
+			set @v_query=concat('insert into data_final_details_t (uniqueid,tier,rate_type,ceiling,rate,call_minutes,hybrid_type,amount,created_at,rate_profile_detail_id,is_vbr,billing_operator,service_id,product,start_date,end_date,deleted_at) values(''',v_uniqueid,''',''',v_tier_code,''',''',v_rate_type,''',',ifnull(v_ceiling,0),',',ifnull(v_rate,0),',',callsapplied,',''',ifnull(v_hybrid_type,'null'),''',',amountapplied,',sysdate(),''',v_rateprofileid,''',''',v_is_vbr,''',''',v_operator_code,''',''',v_serviceid,''',''',v_product_id,''',str_to_date(''',v_start_date,''',''%d/%m/%Y''),str_to_date(''',v_end_date,''',''%d/%m/%Y''), null)');
 			insert into query_log values(v_uniqueid, @v_query, sysdate());
 			prepare stmt from @v_query;
 			execute stmt;
@@ -225,7 +222,7 @@ end loop notvbr_loop;
 set @v_query='update data_final_amount a set a.deleted_at=sysdate()
 where concat(a.billing_operator,a.service_id,date_format(a.start_date,''%d%m%Y''),date_format(a.end_date,''%d%m%Y''))
 IN
-(select Concat(b.operator_code, b.service_code,date_format(b.start_date,''%d%m%Y''),date_format(b.end_date,''%d%m%Y'')) 
+(select Concat(b.operator_code, b.serviceid,date_format(b.start_date,''%d%m%Y''),date_format(b.end_date,''%d%m%Y'')) 
 from vw_call_volume_link b) and a.deleted_at is null';
 prepare stmt from @v_query;
 execute stmt;
@@ -234,36 +231,45 @@ deallocate prepare stmt;
 set @v_query=null;
 /*-------------------------------------------*/
 set @v_query='insert into data_final_amount(billing_operator, billing_operator_name, event_duration_minutes, average_rate, total_amount, service_id, created_at, start_date, end_date)
-select a.operator_code, o.name, sum(a.callduration), round(sum(a.calculated_amount)/sum(a.callduration),2), sum(a.calculated_amount), a.service_code, a.calculated_at, a.start_date, a.end_date
+select a.billing_operator, o.name, sum(a.call_minutes), round(sum(a.amount)/sum(a.call_minutes),2), sum(a.amount), a.service_id, a.created_at, a.start_date, a.end_date
 from data_final_details_t a
 left join
 operators o
-on a.operator_code=o.code
+on a.billing_operator=o.code
 where a.deleted_at is null and 
-Concat(a.operator_code, a.service_code,date_format(a.start_date,''%d%m%Y''),date_format(a.end_date,''%d%m%Y''))
+Concat(a.billing_operator, a.service_id,date_format(a.start_date,''%d%m%Y''),date_format(a.end_date,''%d%m%Y''))
 in 
-(select Concat(v.operator_code,v.service_code,date_format(v.start_date,''%d%m%Y''),date_format(v.end_date,''%d%m%Y'')) 
+(select Concat(v.operator_code,v.serviceid,date_format(v.start_date,''%d%m%Y''),date_format(v.end_date,''%d%m%Y'')) 
 from vw_call_volume_link v)
-group by a.operator_code, o.name, a.service_code, a.calculated_at, a.start_date, a.end_date';
+group by a.billing_operator, o.name, a.service_id, a.created_at, a.start_date, a.end_date';
 prepare stmt from @v_query;
 execute stmt;
 commit;
 deallocate prepare stmt; 
 set @v_query=null;
-/*----------------------------------------------------------------------------------*/	
+/*-------------------------------------------*/	
+set @v_query='update data_final_details_t a, data_final_amount b
+set a.data_final_id= b.id
+where a.billing_operator=b.billing_operator and a.service_id=b.service_id and a.start_date=b.a.start_date and a.end_date=b.end_date';
+prepare stmt from @v_query;
+execute stmt;
+commit;
+deallocate prepare stmt; 
+set @v_query=null;
+/*----------------------------------------------------------------------------------*/
 end;
 /*
 insert into data_final_amount(billing_operator, billing_operator_name, event_duration_minutes, average_rate, total_amount, service_id, created_at, start_date, end_date)
-select a.operator_code, o.name, sum(callduration), round(sum(calculated_amount)/sum(callduration),2), sum(calculated_amount), service_code, calculated_at, start_date, end_date
+select a.operator_code, o.name, sum(callduration), round(sum(calculated_amount)/sum(callduration),2), sum(calculated_amount), serviceid, calculated_at, start_date, end_date
 from data_final_details_t a
 left join
 operators o
 on a.operator_code=b.code
-group by a.operator_code, o.name, service_code, calculated_at, start_date, end_date;
+group by a.operator_code, o.name, serviceid, calculated_at, start_date, end_date;
 commit;
 
 update data_final_details_t a,data_final_amount b
 set a.data_final_id=b.id
-where a.billing_operator=b.operator_code and a.service_id=b.service_code and a.start_date=b.start_date and a.end_date=b.end_date;
+where a.billing_operator=b.operator_code and a.service_id=b.serviceid and a.start_date=b.start_date and a.end_date=b.end_date;
 commit;
 */
